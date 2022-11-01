d9a2095c8b275721a97ed67b9ec11dc84386f121
DibyaLochan-Pahi <dibyalochan.pahi@gmail.com>
2022-11-01 22:34:55 +0530

    Added UC4 Problem Solving using Switch Case

---
+++ UC4_SolvingbySwitchCase/UC4_SolvingbySwitchCase/EmpWageSwitchCase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace UC4_SolvingbySwitchCase
+{
+    internal class EmpWageSwitchCase
+    {
+        public const int empWagePerHour = 20;
+        public EmpWageSwitchCase()
+        {
+            Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
+        }
+        static void Main(string[] args)
+        {
+            EmpWageSwitchCase empWageSwitchCase = new EmpWageSwitchCase();
+            int empHour = 0;
+            Random random = new Random();
+            EmpWageCalculator(empHour, random);
+        }
+
+        private static void EmpWageCalculator(int empHour, Random random)
+        {
+            int count = random.Next(0, 3);
+            switch(count)
+            {
+                case 1:
+                    Console.WriteLine("Employee is Present");
+                    empHour = 8;
+                    break;
+                case 2:
+                    Console.WriteLine("Employee is Half Time");
+                    empHour = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is absent");
+                    empHour = 0;
+                    break;
+            }
+
+            Console.WriteLine("Employee Total Wage Today : " + empHour * empWagePerHour);
+        }
+    }
+}

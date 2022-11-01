218137bc06b5495f02b7847f90dacb5fb6e26427
DibyaLochan-Pahi <dibyalochan.pahi@gmail.com>
2022-11-01 21:51:32 +0530

    Added UC2 Program Calculate Daily Employee Wage

---
+++ UC2_CalculateDailyEmpWage/UC2_CalculateDailyEmpWage/CalculateDailyWage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace UC2_CalculateDailyEmpWage
+{
+    internal class CalculateDailyWage
+    {
+        public const int empWagePerHour = 20;
+        public CalculateDailyWage()
+        {
+            Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
+        }
+        static void Main(string[] args)
+        {
+            CalculateDailyWage calculateDailyWage = new CalculateDailyWage();
+            int empHour=0;
+            Random random=new Random();
+            EmpWageCalculator(empHour,random);
+        }
+
+        private static void EmpWageCalculator(int empHour, Random random)
+        {
+            int count=random.Next(0,2);
+            if (count == 0) 
+            {
+                Console.WriteLine("Employee is Present");
+                empHour = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.WriteLine("Employee Total Wage Today : " + empHour*empWagePerHour);
+        }
+    }
+}

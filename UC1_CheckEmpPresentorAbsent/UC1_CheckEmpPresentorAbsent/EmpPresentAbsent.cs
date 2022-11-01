9bc5873c686ff25f73648ec1c33d9306c20c2917
DibyaLochan-Pahi <dibyalochan.pahi@gmail.com>
2022-11-01 21:44:57 +0530

    Added UC1 Program Check Employee is Present or Absent

---
+++ UC1_CheckEmpPresentorAbsent/UC1_CheckEmpPresentorAbsent/EmpPresentAbsent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UC1_CheckEmpPresentorAbsent
+{
+    internal class EmpPresentAbsent
+    {
+        public  EmpPresentAbsent()
+        {
+            Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
+            Console.WriteLine("---------------------------------------------");
+        }
+        static void Main(string[] args)
+        {
+            EmpPresentAbsent empPresentAbsent = new EmpPresentAbsent();
+            Random random = new Random();
+            int empPresent = 0;
+            EmpStatus(random,empPresent);
+        }
+
+        private static void EmpStatus(Random random, int empPresent)
+        {
+           int check=random.Next(0,2);
+            if(check == empPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}

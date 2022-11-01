909669978d9bcc65687d004c9951f356acf86769
DibyaLochan-Pahi <dibyalochan.pahi@gmail.com>
2022-11-01 22:05:29 +0530

    Added UC3 Program Add Part Time Employee

---
+++ UC3_AddPartTimeWage/UC3_AddPartTimeWage/AddPartTime.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace UC3_AddPartTimeWage
+{
+    internal class AddPartTime
+    {
+        public const int empWagePerHour = 20;
+        public AddPartTime()
+        {
+            Console.WriteLine("WELCOME TO EMPLOYEE WAGE COMPUTATION PROGRAM");
+        }
+        static void Main(string[] args)
+        {
+            AddPartTime addPartTime = new AddPartTime();
+            int empHour = 0;
+            Random random = new Random();
+            EmpWageCalculator(empHour, random);
+        }
+
+        private static void EmpWageCalculator(int empHour, Random random)
+        {
+            int count = random.Next(0, 3);
+            if (count == 0)
+            {
+                Console.WriteLine("Employee is Present");
+                empHour = 8;
+            }
+            else if (count == 1)
+            {
+                Console.WriteLine("Employee is Part Time Emp");
+                empHour = 4;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.WriteLine("Employee Total Wage Today : " + empHour * empWagePerHour);
+        }
+    }
+}
+

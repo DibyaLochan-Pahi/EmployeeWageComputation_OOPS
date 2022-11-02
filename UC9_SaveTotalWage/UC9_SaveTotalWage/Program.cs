fcff9739434d5df86c5405d7e09166f8fb54a6c0
DibyaLochan-Pahi <dibyalochan.pahi@gmail.com>
2022-11-03 00:49:46 +0530

    Added UC9 Program Save Total Wage

---
+++ UC9_SaveTotalWage/UC9_SaveTotalWage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC9_SaveTotalWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilder dMart = new EmpWageBuilder("DMart", 20, 2, 10);
+            EmpWageBuilder reliance = new EmpWageBuilder("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
+        }
+    }
+}

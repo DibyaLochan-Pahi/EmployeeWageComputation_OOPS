549966780df0fb27c1270851eeb933c09f52f03f
DibyaLochan-Pahi <dibyalochan.pahi@gmail.com>
2022-11-03 01:04:33 +0530

    Added UC10 Program Manage Employee Wage of Multiple Company

---
+++ UC10_ManageEmpWageoMultiCompany/UC10_ManageEmpWageoMutltiCompany/EmpWageMain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UC10_ManageEmpWageoMutltiCompany
+{
+    internal class EmpWageMain
+    {
+        public EmpWageMain()
+            {
+                Console.WriteLine("Welome to Employee Wage Program");
+                Console.WriteLine("========================================");
+            }
+            static void Main(string[] args)
+            {
+                EmpWageMain emp = new EmpWageMain(); 
+
+                EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+                empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+                empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+                empWageBuilder.addCompanyEmpWage("TCS", 15, 3, 15);
+                empWageBuilder.addCompanyEmpWage("Samsung", 25, 5, 22);
+                empWageBuilder.addCompanyEmpWage("Facebook", 30, 6, 25);
+                empWageBuilder.computeEmpWage();
+            }
+        
+    }
+}
